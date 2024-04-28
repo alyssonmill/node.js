cbef9faa9d064a0adbf325fb7abab15bcdc55f1e
Alysson Miller <alyssonmillerdev@gmail.com>
2024-04-28 08:38:18 +0100

    Still practing api's using Insomnia

--- Api's on nodejs/script.js
+++ Api's on nodejs/script.js
@@ -1,75 +1,65 @@
-class Car {
-   Brand
-   Model
-   Categorie 
-   Year
-   Quilometragem
-   Value
+// Depois disso o desafio é implementar as APIs: 
+// GET, GET by id, POST, PUT, DELETE 
+// no código usando o typscript.
 
-   constructor(Brand, Model, Categorie, Year, Quilometragem, Value){
-    this.Brand = "";
-    this.Model = "";
-    this.Categorie = "";
-    this.Year = 0;
-    this.Quilometragem = 0;
-    this.Value = 0;
-   }
+class Car {
+    constructor(Brand, Model, Categorie, Year, Quilometragem, Value) {
+        this.Brand = Brand;
+        this.Model = Model;
+        this.Categorie = Categorie;
+        this.Year = Year;
+        this.Quilometragem = Quilometragem;
+        this.Value = Value;
+    }
 }
 
 function Get() {
 
-    console.log("Hello")
+    console.log("Test")
 
-    return fetch('https://apigenerator.dronahq.com/api/ha0iGO11/Car')
-  
-      .then((res) => res.json(
-        console.log("Inside of then"),
-        console.log(res.json)
-      ))
-  
-      .then((data) => console.log(data));
-  
-  }
+    return fetch('https://apigenerator.dronahq.com/api/qD9FMxBL/Cars')
 
-  async function postData(url = '', data = {}) {
+        .then((res) => res.json(
+            console.log("Inside of then"),
+            console.log(res.json)
+        ))
 
-    console.log("Hello World")
+        .then((data) => console.log(data));
 
-    const response = await fetch(url, {
-  
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-  
-      mode: 'cors',
-  
-      cache: 'no-cache',
-  
-      credentials: 'same-origin',
-  
-      headers: {
-  
-        'Content-Type': 'application/json'
-  
-      },
-  
-      redirect: 'follow',
-  
-      referrerPolicy: 'no-referrer',
-  
-      body: JSON.stringify(data)
-  
-    });
-  
-    return console.log(response.json());
-  
-  }
-  
-  
-  postData('https://apigenerator.dronahq.com/api/ha0iGO11/Car', Car /* OBJETO A SER USADO */)
-  
-    .then((Car) => {
-  
-      console.log(Car);  
-  
-  });
-  
-  
+}
+
+async function postData(url = '', data = {}) {
+
+    console.log("Test")
+
+    try {
+        const response = await fetch(url, {
+
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(data)
+        });
+
+        let responseData = await response.json()
+        console.log(responseData);
+
+        Get();
+
+    } catch (error) {
+        console.error('Error posting data:', error);
+    }
+}
+
+const carData = new Car("Toyota", "Corolla", "Sedan", 2022, 10000, 25000);
+
+postData('https://apigenerator.dronahq.com/api/ha0iGO11/Car', carData);
+
+
