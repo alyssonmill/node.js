4ca0fa733adfb948ff96199b34a68c9825c91ec1
Alysson Miller <alyssonmillerdev@gmail.com>
2024-04-28 08:58:24 +0100

    Getting in truble on api's

--- Api's on nodejs/script.js
+++ Api's on nodejs/script.js
@@ -51,6 +51,64 @@ async function postData(url = '', data = {}) {
         let responseData = await response.json()
         console.log(responseData);
 
+    } catch (error) {
+        console.error('Error posting data:', error);
+    }
+}
+
+async function putData(url = '', data = {}) {
+
+    console.log("Test")
+
+    try {
+        const response = await fetch(url, {
+
+            method: 'PUT', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(data)
+        });
+
+        let responseData = await response.json()
+        console.log(responseData);
+
+        Get();
+
+    } catch (error) {
+        console.error('Error posting data:', error);
+    }
+}
+
+async function deleteData(url = '', data = {}) {
+
+    console.log("Test")
+
+    try {
+        const response = await fetch(url, {
+
+            method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(data)
+        });
+
+        let responseData = await response.json()
+        console.log(responseData);
+
         Get();
 
     } catch (error) {
@@ -59,7 +117,27 @@ async function postData(url = '', data = {}) {
 }
 
 const carData = new Car("Toyota", "Corolla", "Sedan", 2022, 10000, 25000);
+const updatedCarData = {
+    Brand: "Toyota",
+    Model: "Camry",
+    Type: "Sedan",
+    Year: 2023,
+    Quilometragem: 12000,
+    Value: 27000
+};
+const deleteCarData = {
+    Brand: "Volvo",
+    Model: "CL200",
+    Type: "Luxe",
+    Year: 2013,
+    Quilometragem: 0,
+    Value: 200000
+};
+
+
 
 postData('https://apigenerator.dronahq.com/api/ha0iGO11/Car', carData);
+putData('apigenerator.dronahq.com/api/qD9FMxBL/Cars/1', updatedCarData)
+// deleteData('apigenerator.dronahq.com/api/qD9FMxBL/Cars/1', deleteCarData )
 
 
